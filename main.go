a0e98b48e2eb0c666acf807a27a3d48343d9a2f3
Vikas <vikas3517@gmail.com>
2023-09-21 19:34:21 +0530

    Initialization of go gin

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello Go Gin")
+	r := gin.Default()
+	r.GET("/", func (c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello to Go Gin world"})
+	})
+	r.Run()
 }
