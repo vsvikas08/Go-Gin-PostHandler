70b8cb0ab1597c1007bfc4e8e0416287d3ba7f04
Vikas <vikas3517@gmail.com>
2023-09-22 12:05:35 +0530

    Route for post method and DB as file json

--- main.go
+++ main.go
@@ -7,11 +7,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func getPost(c *gin.Context) {
+
+}
+func createPost(c *gin.Context) {
+	var newPost Post
+	if err := c.BindJSON(&newPost); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Bad request from user"})
+		return
+	}
+	c.JSON(http.StatusOK,gin.H{"data": newPost})
+}
+func updatePost(c *gin.Context) {
+
+}
 func main() {
 	fmt.Println("Hello Go Gin")
 	r := gin.Default()
 	r.GET("/", func (c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello to Go Gin world"})
 	})
+
+	r.GET("/post",getPost)
+	r.POST("/post",createPost)
+	r.PUT("/post",updatePost)
+	
+
 	r.Run()
 }
---
+++ types.go
@@ -0,0 +1,24 @@
+package main
+
+type Post struct {
+	Id       string    `json:"id"`
+	Title    string    `json:"title"`
+	Date     string    `json:"date"`
+	Author   User      `json:"author"`
+	Content  string    `json:"content"`
+	Comments []Comment `json:"comments"`
+}
+
+type Comment struct {
+	PostId  string `json:"post_id"`
+	User    User   `json:"user"`
+	Comment string `json:"comment"`
+}
+
+type User struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	password string `json:"password"`
+	Image    string `json:"img,omitempty"`
+}
